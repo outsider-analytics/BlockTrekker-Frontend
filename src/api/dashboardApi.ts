96769f23c86a93181cc6f1a431e52ca3eedf35bf
Ian-Bright <ianbrighton1@gmail.com>
2023-03-26 07:49:44 -0600

    resolve lint warnings

--- src/api/dashboardApi.ts
+++ src/api/dashboardApi.ts
@@ -16,8 +16,10 @@ export const getDashboard = (user: string): Promise<Response> =>
 export const saveDashboard = (user: string, payload: any): Promise<Response> =>
   serverRequest(`dashboard/save/${user}`, RequestTypes.Post, payload);
 
-// TODO: Change from any
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export const updateTextWidget = (user: string, payload: any): Promise<Response> =>
+export const updateTextWidget = (
+  user: string,
+  // TODO: Change from any
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+  payload: any
+): Promise<Response> =>
   serverRequest(`dashboard/widget/${user}`, RequestTypes.Put, payload);
-
